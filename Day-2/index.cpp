f7f8996d7041c14c26c51132b9effab12a937107
zennyMe17 <gowda.hemanth.1718@gmail.com>
2024-10-01 14:36:13 +0530

    Fresh start

---
+++ Day-2/index.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+          //cout << "Namaste Bharat\n";
+          //cout << "Hello World\n";
+          // cout <<"Enter a number\n" <<endl;
+          // int a;
+          // cin >> a;
+          // cout << "You entered " << a <<endl;
+
+
+  // int a = 5;
+  // cout << "Value of a is  : " << a << endl;  
+
+  // char ch = 'a';
+  // cout << "Value of ch is : " << ch << endl;
+
+  // bool flag = true;
+  // cout << "Value of flag is : " << flag << endl;
+
+  // char ch = 'a';
+  // cout << "value is = " << ch << endl;
+
+  // int a = 5;
+  // cout << sizeof(a) << endl;
+
+  // char ch = 97;
+  // cout << "ch is :" << ch << endl;
+
+  float a = 5;
+  float b = 2;
+  cout << (a == b) << endl;
+  
+}
